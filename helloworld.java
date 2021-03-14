64cc438e6498d52c2d77ac5ec4c978a08cf1e5e8
nramyacta <nramya3000@gmail.com>
2021-03-14 12:20:30 +0000

    checking for git demo

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+/**
+*Name: Ramya
+*Description: compilation and running test
+*Date: 3/12/2021
+*/
+class helloworld{
+public static void main(String[]args){
+System.out.println("Hello world");
+}
+}
